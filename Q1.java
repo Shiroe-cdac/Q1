3867899d9c6201856dc1e533eb70110984a5ed11
Vrushabh <vrushabhvaidya86@gmail.com>
2022-03-05 10:02:00 +0530

    Initial Commit

---
+++ Q1.java
@@ -0,0 +1,26 @@
+
+public class Programs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		  System.out.println("Hello"); System.out.println("VNV");
+		  
+		  
+		  Scanner input=new Scanner(System.in); System.out.println("Enter 1st no. :");
+		  int a=input.nextInt(); System.out.println("Enter 2nd no. :"); int
+		  b=input.nextInt(); System.out.println("Enter 3rd no. :"); int
+		  c=input.nextInt();
+		  
+		  
+		  System.out.println("Output="+(-5+8*6));
+		  System.out.println("Output="+((55+9)%9));
+		  System.out.println("Output="+(20+-3*5/8));
+		  
+		  
+		  Scanner input=new Scanner(System.in); System.out.println("Enter no. :"); int
+		  a=input.nextInt(); for(int i=1;i<=10;i++) {
+		  System.out.println(a+" x "+i+" = "+a*i); }
+          }
+        } 
